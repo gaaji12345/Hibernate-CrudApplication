9a5187a708e69121c77d3c90774f2166084a9d1c
gaji12345 <gajimax12345@gmail.com>
2024-02-15 15:44:54 +0530

    test

---
+++ src/main/java/controller/CustomerFormController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class  CustomerFormController {
+
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtTel;
+    public TableView tblCustomers;
+    public TableColumn tblID;
+    public TableColumn tblName;
+    public TableColumn tblAddress;
+    public TableColumn tblTel;
+
+    public void txtSearchOn_Action(ActionEvent actionEvent) {
+    }
+
+    public void save_INAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void update_OnAction(ActionEvent actionEvent) {
+    }
+}
